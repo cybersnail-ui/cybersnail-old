1dd7de223275dc7ddac9b346e29c10f1c7f342aa
Lillian Raphael <lillianpetals@gmail.com>
2019-11-23 16:21:57 -0500

    fixed mobile view, created js directory
    fixed mobile view, created js directory

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", ready);
+
+function ready()
+{
+
+    window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    function myFunction()
+    {
+        if (window.pageYOffset >= sticky) 
+        {
+            navbar.classList.add("sticky")
+        }
+        else 
+        {
+            navbar.classList.remove("sticky");
+        }
+    }
+}
